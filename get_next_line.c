8298544113e37ae584259a5d9678682ca23dffc7
Abel Ferrer <abeferrer7@gmail.com>
2025-03-18 20:05:44 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,18 +12,6 @@
 
 #include "get_next_line.h"
 
-// funcion que lea el .txt y que lo añada con strjoin a tu buffer estatico.
-// funcion para escribir tu linea con substr.
-// funcion para limpiar el buffer estatico con substr despues de escribir una linea.
-// hacer la funcion principal de get_next_line.
-
-// Este get_next_line se compone de cuatro funciones:
-
-// in_buffer(fd, buffer): Lee del archivo y guarda en buffer.
-// procc_line(buffer): Extrae una línea completa de buffer.
-// sep_line(buffer): Elimina la línea ya procesada y guarda lo restante.
-// get_next_line(fd): Llama a las otras funciones y maneja la memoria.
-
 static char *read_buffer(int fd, char *buffer)
 {
     char    *buff;
